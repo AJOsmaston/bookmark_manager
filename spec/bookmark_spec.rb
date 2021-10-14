ab5e9929d20d3e6f63a1326723718d18579d2a1c
AJOsmaston <dajoxx@gmail.com>
2021-10-14 10:43:04 +0100

    refactored with persisted_data helper method

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require_relative 'setup_test_database'
 
 describe Bookmark do
   describe '.all' do 
@@ -6,24 +7,25 @@ describe Bookmark do
       bookmark = Bookmark.create(url: "http://www.google.com", title: "Google")
       Bookmark.create(url: "http://www.amazon.com", title: "Amazon")
       Bookmark.create(url: "http://www.twitter.com", title: "Twitter")
+      persisted_data = persisted_data(id: bookmark.id)
       
       bookmarks = Bookmark.all
 
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      expect(bookmarks.first.id).to eq bookmark.id
-      expect(bookmarks.first.title).to eq "Google"
-      expect(bookmarks.first.url).to eq "http://www.google.com"
+      expect(persisted_data['id']).to eq bookmark.id
+      expect(persisted_data['title']).to eq "Google"
+      expect(persisted_data['url']).to eq "http://www.google.com"
     end
   end
 
   describe '.create' do
     it 'creates a bookmark' do
       bookmark = Bookmark.create(url: "http://www.google.com", title: "Google")
-      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+      persisted_data = persisted_data(id: bookmark.id)
 
       expect(bookmark).to be_a Bookmark
-      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.id).to eq persisted_data['id']
       expect(bookmark.title).to eq 'Google'
       expect(bookmark.url).to eq "http://www.google.com"
     end
